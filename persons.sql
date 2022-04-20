911a4b00c45fa6ea2476ef717dc168cc5888cb1f
Toros <toros_kutlu@hotmail.com>
2022-04-20 16:07:48 +0200

    Add files via upload

---
+++ persons.sql
@@ -0,0 +1,16 @@
+DROP DATABASE Persons;
+
+CREATE DATABASE Persons;
+
+USE Persons;
+/*
+CREATE TABLE IF NOT EXISTS Persons (
+    person_id INT AUTO_INCREMENT PRIMARY KEY,
+    person_first_name VARCHAR(255),
+    person_last_name VARCHAR(255),
+    person_birthdate DATE,
+    person_address VARCHAR(255)
+);
+*/
+
+SELECT * FROM Persons;
